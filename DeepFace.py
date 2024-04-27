2bf7b7885c45bbd3258aa0a2e119764291cc6bfe
sherifalafifioo1 <147742240+sherifalafifioo1@users.noreply.github.com>
2024-04-27 19:41:37 +0300

    Update DeepFace.py

--- DeepFace.py
+++ DeepFace.py
@@ -14,7 +14,7 @@ from deepface import DeepFace
 def match_user_id_pic(ID_img, pic):
 
     # Verify the images using DeepFace
-    obj = DeepFace.verify(ID_img, pic,  model_name = 'Facenet', detector_backend = 'retinaface')
+    obj = DeepFace.verify(ID_img, pic,  model_name = 'Facenet', detector_backend = 'opencv')
 
     
     return obj['verified']
