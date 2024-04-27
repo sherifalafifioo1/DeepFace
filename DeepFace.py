2b71e78242df92607756ae5f9f8e5b85ce47112a
sherifalafifioo1 <147742240+sherifalafifioo1@users.noreply.github.com>
2024-04-27 07:43:13 +0300

    Add files via upload

---
+++ DeepFace.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+import os
+import cv2 
+import numpy as np
+from deepface import DeepFace
+
+
+def match_user_id_pic(ID_img, pic):
+
+    # Verify the images using DeepFace
+    obj = DeepFace.verify(ID_img, pic,  model_name = 'ArcFace', detector_backend = 'retinaface')
+
+    
+    return obj['verified']
+#-----------------------------------------------
+
